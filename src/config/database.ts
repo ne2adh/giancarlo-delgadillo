290e921f481bbda4135be074e1938d7323def762
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2024-11-24 18:30:54 -0400

    fix database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('proyecto', 'root', '', {
-  host: 'localhost',
+export const sequelize = new Sequelize('be2skqptzcwdsfd4gn6j', 'uypyriigdhk7krix', 'KnDcWK7OgV5kYhLbWXRF', {
+  host: 'be2skqptzcwdsfd4gn6j-mysql.services.clever-cloud.com',
   dialect: 'mysql',
 });
